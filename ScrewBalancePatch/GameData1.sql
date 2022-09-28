7b03072cba7d27be925a3c234471a982955e9468
Matthew Williams <mattcrwi@gmail.com>
2022-09-28 12:18:12 -0400

    save changes

--- ScrewBalancePatch/GameData1.sql
+++ ScrewBalancePatch/GameData1.sql
@@ -10,3 +10,6 @@
 UPDATE ScoringLineItems
 SET Multiplier = 8.0
 WHERE name = 'LOC_LINE_ITEM_WONDERS_NAME';
+
+INSERT INTO ModifierArguments (ModifierId, Name, Type, Value, Extra, SecondExtra)
+VALUES('WORK_ETHIC_ADJACENCY_PRODUCTION_2', 'ScalingFactor', 'ARGTYPE_IDENTITY', '50', null, null);
